3f61619e732c163876eb7a8fd71793fb9eecf48a
Andrei <andrei.baditoiu@gmail.com>
2024-09-30 17:10:53 +0300

    Modified: settings, urls, views (py); added:about, index (html).

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+import movies
+
+
 # Create your views here.
+
+def index(request):
+    context = {
+        'movies':['gladiator', 'terminator 2', 'contact' ]
+    }
+    return render(request, 'index.html', context )
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
